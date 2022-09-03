b4326f8331a5eae535badbb216366a19bfeb72e2
Bryan Sory <superbee45@gmail.com>
2022-09-02 21:37:08 -0500

    try object for start of array

--- fixLengthPalindrome.js
+++ fixLengthPalindrome.js
@@ -7,29 +7,54 @@
     //1: try to spit out all the numbers of int length sequentially.
     //2. implement logic to sort/filter? that array for palindromes
     //3. take the ith index from elements of queries array and further filter 
-console.log('hello world')
-let generator = function(intLength){
-    var foo = [];
+//Hard problems:
+//1. intLength is misleading me. It is the length of a (do we use Base 10?) base 10 number, e.g. 3 is XXX. Computers use binary.
+//..con't: 'given a number of digits, generate the array of all the numbers that have that number of digits...'3' gives [100,101...999]
 
-    for (var i = 1; i <= intLength; i++) {
+
+/**
+ * @param {number[]} queries
+ * @param {number} intLength
+ * @return {number[]}
+ */
+ var kthPalindrome = function(queries, intLength) {
+    var foo = [];
+    const startNumber = {
+        1 : 1,
+        2 : 10,
+        3 : 100,
+        4 : 1000,
+        5 : 10000,
+        6 : 100000,
+        7 : 1000000
+    }
+    for (var i = startNumber(intLength); i < startNumber(intLength+1); i++) {
         const a = i.toString().split('').join('');
         const b = i.toString().split('').reverse().join('')
         if (a === b) {
             foo.push(i)
-        }
-         
-    }
+        }  
     return foo;
-}
+    };
+ }
 
 
+//below will return an array of palindromes up to intLength; not really the point ;
 // var kthPalindrome = function(queries, intLength) {
-//     let generator = function(intLength){
-//         var foo = [];
-
-//         for (var i = 1; i <= intLength; i++) {
-//             foo.push(i);
-//             console.log(foo)
-//         }
-//     }
-// }
+    // 
+        // 
+        // const fan = [];
+    // 
+        // for (var i = 1; i <= intLength; i++) {
+            // const a = i.toString().split('').join('');
+            // const b = i.toString().split('').reverse().join('')
+            // if (a === b) {
+                // foo.push(i)
+            // }     
+        // }
+        // for(query of queries) {
+            // fan.push(foo.indexOf(query))
+        // }
+    // 
+    // console.log(foo)    
+    // // return fan;   
